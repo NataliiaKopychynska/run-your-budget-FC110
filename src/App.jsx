8405cf002cea74ce488a0bd40d0cee5abe695567
anna_vezhdel <avezhdel@gmail.com>
2025-04-18 23:03:05 +0200

    add routes and register form

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./pages/LoginPage/LoginPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
 
-function App() {
+const App = () => {
   return (
     <>
-      <LoginPage />
+      <Routes>
+        <Route path="register" element={<RegisterPage />} />
+        <Route path="login" element={<LoginPage />} />
+      </Routes>
     </>
   );
-}
+};
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,15 @@ import App from "./App.jsx";
 import { Provider } from "react-redux";
 import { store, persistor } from "./redux/store";
 import { PersistGate } from "redux-persist/integration/react";
+import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <App />
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
       </PersistGate>
     </Provider>
   </StrictMode>
