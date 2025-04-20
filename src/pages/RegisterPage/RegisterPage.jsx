8dc2906846908aabf67d13f13e620f3c6ae337f4
anna_vezhdel <avezhdel@gmail.com>
2025-04-20 10:50:34 +0200

    register mockapi, styles

--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,8 +1,9 @@
 import RegisterForm from "../../components/RegisterForm/RegisterForm";
+import s from "./RegisterPage.module.css";
 
 function RegisterPage() {
   return (
-    <div>
+    <div className={s.reg_page}>
       <RegisterForm />
     </div>
   );
