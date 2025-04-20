58faf2a4c97de19081b07c1e86cc51342d433ee1
Teslya Stepan <stepan199812@gmail.com>
2025-04-20 10:01:32 +0300

    add balance

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import Balance from "./components/Balance/Balance";
 
 const App = () => {
   return (
     <>
+      <Balance />
       <Routes>
         <Route path="register" element={<RegisterPage />} />
         <Route path="login" element={<LoginPage />} />
--- src/redux/categories/selector.js
+++ src/redux/categories/selector.js
@@ -1,4 +1,3 @@
-export const selectTransactions = (state) => state.home.transactions.items;
 export const selectCategories = (state) => state.home.categories.title;
 export const selectIsLoading = (state) => state.home.isLoading;
 export const selectIsError = (state) => state.home.isError;
