79df5ea22aa24d86d9433452dc520f705a8ca9d5
Teslya Stepan <stepan199812@gmail.com>
2025-04-24 11:21:15 +0300

    fix dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,20 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
 import HomePage from "./pages/HomePage/HomePage";
-import BackgroundGradientTablet from "./components/BackgroundGradient/BackgroundGradient";
-// import Currency from "./components/Currency/Currency";
 import CurrencyPage from "./pages/CurrencyPage/CurrencyPage";
+import DashboardPage from "./pages/DashboardPage/DashboardPage";
 import StatisticsTab from "./pages/StatisticsPage/StatisticsTab";
 
+import BackgroundGradientTablet from "./components/BackgroundGradient/BackgroundGradient";
 import DeleteModal from "./components/DeleteModal/DeleteModal";
-
-// import ModalAddTransaction from "./components/ModalAddTransaction/ModalAddTransaction";
 import Loader from "./components/Loader/Loader";
-
-import { useMediaQuery } from "react-responsive";
-// import Navigation from "./components/Navigation/Navigation";
-// import HeaderComponent from "./components/Header/HeaderComponent";
-import DashboardPage from "./pages/DashboardPage/DashboardPage";
 import { RestrictedRoute } from "./components/Routes/RestrictedRoute";
 import { PrivateRoute } from "./components/Routes/PrivateRoute";
 
 const App = () => {
-  const isDesktopOrTablet = useMediaQuery({ query: "(min-width: 768px)" });
   return (
     <>
       <BackgroundGradientTablet />
@@ -30,45 +23,27 @@ const App = () => {
 
       <Routes>
         <Route
-          path="/"
-          element={
-            <PrivateRoute component={<HomePage />} redirectTo="/login" />
-          }
-        ></Route>
-        <Route
-          path="/home"
-          element={
-            <PrivateRoute component={<HomePage />} redirectTo="/login" />
-          }
-        ></Route>
-        <Route
           path="register"
           element={
-            <RestrictedRoute component={<RegisterPage />} redirectTo="/" />
+            <RestrictedRoute component={<RegisterPage />} redirectTo="/home" />
           }
         />
         <Route
           path="login"
-          element={<RestrictedRoute component={<LoginPage />} redirectTo="/" />}
+          element={
+            <RestrictedRoute component={<LoginPage />} redirectTo="/home" />
+          }
         />
         <Route
-          path="dashboard"
+          path="/"
           element={
             <PrivateRoute component={<DashboardPage />} redirectTo="/login" />
           }
-        />
-        <Route
-          path="currency"
-          element={
-            <PrivateRoute component={<CurrencyPage />} redirectTo="/login" />
-          }
-        />
-        <Route
-          path="statistics"
-          element={
-            <PrivateRoute component={<StatisticsTab />} redirectTo="/login" />
-          }
-        />
+        >
+          <Route path="home" element={<HomePage />} />
+          <Route path="statistics" element={<StatisticsTab />} />
+          <Route path="currency" element={<CurrencyPage />} />
+        </Route>
       </Routes>
     </>
   );
