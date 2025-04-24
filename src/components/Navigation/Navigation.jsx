a88bc773d55558e7d53d0fc51e66e2a8c579175d
maks2802 <komrakovmax2004@gmail.com>
2025-04-24 11:04:32 +0300

    fix navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -5,22 +5,19 @@ import s from "./Navigation.module.css";
 const Navigation = () => {
   return (
     <nav className={s.nav}>
-      <NavLink className={s.link} to={"/dashboard/home"}>
+      <NavLink className={s.link} to="/home">
         <svg className={s.icon} width={38} height={38}>
           <use href="/public/img/icons.svg#icon-baseline-home-24px-3" />
         </svg>
         <p className={s.navText}>Home</p>
       </NavLink>
-      <NavLink className={s.link} to={"/dashboard/statistics"}>
+      <NavLink className={s.link} to="/statistics">
         <svg className={s.icon} width={38} height={38}>
           <use href="/public/img/icons.svg#icon-baseline-timeline-24px-1" />
         </svg>
         <p className={s.navText}>Statistics</p>
       </NavLink>
-      <NavLink
-        className={clsx(s.link, s.mobileOnly)}
-        to={"/dashboard/currency"}
-      >
+      <NavLink className={clsx(s.link, s.mobileOnly)} to="/currency">
         <svg className={s.icon} width={38} height={38}>
           <use href="/public/img/icons.svg#icon-baseline-timeline-24px-4" />
         </svg>
