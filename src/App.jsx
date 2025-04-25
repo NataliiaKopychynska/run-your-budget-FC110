3e145112b0c30c9a154c74a79a642082a0f55783
Teslya Stepan <stepan199812@gmail.com>
2025-04-25 14:49:33 +0300

    Loader on reload

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
 
 import LoginPage from "./pages/LoginPage/LoginPage";
 import RegisterPage from "./pages/RegisterPage/RegisterPage";
@@ -14,9 +17,23 @@ import Loader from "./components/Loader/Loader";
 
 import { RestrictedRoute } from "./components/Routes/RestrictedRoute";
 import { PrivateRoute } from "./components/Routes/PrivateRoute";
-
+import { fetchTransactions } from "./redux/transactions/operations";
 
 const App = () => {
+  const dispatch = useDispatch();
+  const [isInitializing, setIsInitializing] = useState(true);
+
+  useEffect(() => {
+    const init = async () => {
+      await dispatch(fetchTransactions());
+      setIsInitializing(false);
+    };
+    init();
+  }, [dispatch]);
+
+  if (isInitializing) {
+    return <Loader />;
+  }
   return (
     <>
       <BackgroundGradientTablet />
