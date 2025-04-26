e3f49cc78b5b299577f493832c43c1b6ada96c6f
Ievgen Osadchyi <osadchyi15@gmail.com>
2025-04-27 00:03:16 +0200

    tranaction filter

--- src/components/Transactions/TransactionsList/TransactionsList.jsx
+++ src/components/Transactions/TransactionsList/TransactionsList.jsx
@@ -2,31 +2,85 @@ import { useDispatch, useSelector } from "react-redux";
 import { selectTransactions } from "../../../redux/transactions/selectors";
 import TransactionsItem from "../TransactionsItem/TransactionsItem";
 import s from "../Transactions.module.css";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { fetchTransactions } from "../../../redux/transactions/operations";
 import { selectIsLoading } from "../../../redux/global/selectors";
+import { Field, Form, Formik } from "formik";
+import clsx from "clsx";
 
 const TransactionsList = () => {
   const isLoading = useSelector(selectIsLoading);
   const dispatch = useDispatch();
   const transactionsList = useSelector(selectTransactions);
+  const [sortBy, setSortBy] = useState("_id");
+  const [sortOrder, setSortOrder] = useState("asc");
 
   useEffect(() => {
-    dispatch(fetchTransactions());
-  }, [dispatch]);
+    dispatch(fetchTransactions({ sortBy, sortOrder }));
+  }, [dispatch, sortBy, sortOrder]);
 
   const filteredTransactions = transactionsList.filter(
     (transaction) => transaction && transaction._id
   );
 
+  function handleSortClick(e) {
+    setSortBy(e.target.name);
+    setSortOrder((prevOrder) => (prevOrder === "asc" ? "desc" : "asc"));
+  }
+
   return (
     <>
       <div className={s.transactionListHeader}>
-        <p className={s.transactionsListHeaderItem}>Date</p>
-        <p className={s.transactionsListHeaderItem}>Type</p>
-        <p className={s.transactionsListHeaderItem}>Category</p>
-        <p className={s.transactionsListHeaderItem}>Comment</p>
-        <p className={s.transactionsListHeaderItem}>Sum</p>
+        <input
+          className={clsx(
+            s.transactionsListHeaderItem,
+            sortBy === "date" && s.sorting
+          )}
+          type="button"
+          name="date"
+          value="Date ⇅"
+          onClick={(e) => handleSortClick(e)}
+        />
+        <input
+          className={clsx(
+            s.transactionsListHeaderItem,
+            sortBy === "type" && s.sorting
+          )}
+          type="button"
+          name="type"
+          value="Type ⇅"
+          onClick={(e) => handleSortClick(e)}
+        />
+        <input
+          className={clsx(
+            s.transactionsListHeaderItem,
+            sortBy === "category" && s.sorting
+          )}
+          type="button"
+          name="category"
+          value="Category ⇅"
+          onClick={(e) => handleSortClick(e)}
+        />
+        <input
+          className={clsx(
+            s.transactionsListHeaderItem,
+            sortBy === "comment" && s.sorting
+          )}
+          type="button"
+          name="comment"
+          value="Comment ⇅"
+          onClick={(e) => handleSortClick(e)}
+        />
+        <input
+          className={clsx(
+            s.transactionsListHeaderItem,
+            sortBy === "sum" && s.sorting
+          )}
+          type="button"
+          name="sum"
+          value="Sum ⇅"
+          onClick={(e) => handleSortClick(e)}
+        />
         <p className={s.transactionsListHeaderItem}></p>
       </div>
 
