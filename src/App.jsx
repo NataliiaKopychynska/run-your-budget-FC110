880b89f747064101fa2065a0043189af968fe812
anna_vezhdel <avezhdel@gmail.com>
2025-04-27 17:37:54 +0200

    add notFound page

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import Loader from "./components/Loader/Loader";
 import { RestrictedRoute } from "./components/Routes/RestrictedRoute";
 import { PrivateRoute } from "./components/Routes/PrivateRoute";
 import { fetchTransactions } from "./redux/transactions/operations";
-import SomethingWrong from "./components/SomethingWrong/SomethingWrong";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
 const App = () => {
   const dispatch = useDispatch();
@@ -64,7 +64,7 @@ const App = () => {
           <Route path="statistics" element={<StatisticsTab />} />
           <Route path="currency" element={<CurrencyPage />} />
         </Route>
-        <Route path="*" element={<SomethingWrong />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
   );
