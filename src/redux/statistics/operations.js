f9c91e8107a2b14cab9b82f0b7759db8f1cc1461
Andrii Ishchuk <a_ishchuk22@ukr.net>
2025-04-28 13:50:20 +0300

    shadow and styles issues

--- src/redux/statistics/operations.js
+++ src/redux/statistics/operations.js
@@ -1,6 +1,5 @@
 import axios from "axios";
 import { createAsyncThunk } from "@reduxjs/toolkit";
-// import { fetchTransactions } from "../transactions/operations";
 
 export const runBudgetApi = axios.create({
   baseURL: "https://moneyguard-group-06.onrender.com/",
@@ -24,7 +23,6 @@ export const getPeriodTransactions = createAsyncThunk(
   async (period, thunkAPI) => {
     try {
       const { month, year } = period;
-
       const { data } = await runBudgetApi.get("/transactions");
 
       const filteredData = data.data.data.filter((transaction) => {
@@ -35,11 +33,9 @@ export const getPeriodTransactions = createAsyncThunk(
         return transactionMonth === month && transactionYear === year;
       });
 
-      console.log(filteredData);
-
       const categoriesSummary = [];
-      let incomeSummary = 0;
       let expenseSummary = 0;
+      let incomeSummary = 0;
 
       filteredData.forEach((transaction) => {
         const { sum, type, category } = transaction;
@@ -80,68 +76,3 @@ export const getPeriodTransactions = createAsyncThunk(
     }
   }
 );
-
-// export const runBudgetApi = axios.create({
-//   baseURL: "https://6802ad240a99cb7408ea3ab1.mockapi.io/budget",
-// });
-
-// export const getPeriodTransactions = createAsyncThunk(
-//   "transactions/getPeriodTransactions",
-//   async (period, thunkAPI) => {
-//     try {
-//       const { month, year } = period;
-
-//       const { data } = await runBudgetApi.get("/transactions");
-
-//       const filteredData = data.filter((transaction) => {
-//         const transactionDate = new Date(transaction.date);
-//         const transactionMonth = transactionDate.getMonth() + 1;
-//         const transactionYear = transactionDate.getFullYear();
-
-//         return transactionMonth === month && transactionYear === year;
-//       });
-
-//       const categoriesSummary = [];
-//       let incomeSummary = 0;
-//       let expenseSummary = 0;
-
-//       filteredData.forEach((transaction) => {
-//         const { sum, type, category } = transaction;
-//         const amount = Number(sum);
-
-//         if (type === true) {
-//           incomeSummary += amount;
-//         } else {
-//           expenseSummary += amount;
-//         }
-
-//         const transactionType = type === true ? "INCOME" : "EXPENSE";
-//         const existingCategory = categoriesSummary.find(
-//           (cat) => cat.name === category && cat.type === transactionType
-//         );
-
-//         if (existingCategory) {
-//           existingCategory.total += amount;
-//         } else {
-//           categoriesSummary.push({
-//             name: category,
-//             type: transactionType,
-//             total: amount,
-//           });
-//         }
-//       });
-
-//       const periodTotal = incomeSummary - expenseSummary;
-//       thunkAPI.dispatch(fetchTransactions());
-
-//       return {
-//         categoriesSummary,
-//         incomeSummary,
-//         expenseSummary,
-//         periodTotal,
-//       };
-//     } catch (error) {
-//       return thunkAPI.rejectWithValue(error.message);
-//     }
-//   }
-// );
