5c40379de1dfdc24610cc2a044993a216bbe6ad0
Andrii Ishchuk <a_ishchuk22@ukr.net>
2025-04-29 11:55:32 +0300

    fixed notransactions and tableheight

--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -20,19 +20,22 @@ const Chart = () => {
   const categoriesNames = categoriesSummary.filter(
     category => category.type === 'EXPENSE'
   );
-  if (categoriesNames.length === 0) {
-    return (
-      <div className={s.noTransactionsBox}>
-        <p className={s.noTransactionsText}>There are no transactions added for the selected period</p>
-      </div>
-    );
-  }
 
-   const formatNumber = sum => {
+  const formatNumber = sum => {
     const [integerPart, decimalPart] = sum.toFixed(2).split('.');
     const formattedInteger = integerPart.replace(/\B(?=(\d{3})+(?!\d))/g, ' ');
     return `${formattedInteger}.${decimalPart}`;
   };
+
+
+  if (categoriesNames.length === 0) {
+    return (
+      <div className={s.noTransactionsBox}>
+        <p className={s.noTransactionsText}>There are no expenses for the selected period</p>
+        <p className={s.noTransactionsText}>Income for this month: <br />₴ {formatNumber(periodTotal)}</p>
+      </div>
+    );
+  }
 
   const data = {
     labels: categoriesNames.map(category => category.name),
